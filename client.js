6282a81fd0844d9f82b7a4d9b74ef938021ef7ad
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-07-22 20:42:55 -0500

    button functionality tested.

--- client.js
+++ client.js
@@ -3,11 +3,18 @@ $(document).ready(readyNow);
 // console.log('javascript works');
 
 function readyNow(){
-    console.log('jquery works!');
+    //console.log('jquery works!');
+    //click events:
+    $('#addEmployeeButton').on('click', addEmployee);
+
 
 };
 
 let employees = [];
+
+function addEmployee(){
+    console.log('button');
+}// end of addEmployee function
 
 function createEmployee(employeeFirstName, lastName, idNumber, jobTitle, annualSalary){
     console.log('in the createEmployee function', employeeFirstName, lastName, idNumber, jobTitle, annualSalary )
