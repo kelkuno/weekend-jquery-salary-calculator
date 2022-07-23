baac5b3918a4dabd3da085e60205ecdeb7463607
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-07-22 21:51:54 -0500

    table data appended complete.

--- client.js
+++ client.js
@@ -13,6 +13,9 @@ function readyNow(){
 let employees = [];
 
 function addEmployee(){
+    // let el = $('#displayEmployee');
+    // el.empty();
+
     console.log('button');
     //store user input into variables 
     let employeeFirstName = $('#firstNameInput').val();
@@ -26,7 +29,26 @@ function addEmployee(){
         //call createEmployee function w user input
         createEmployee(employeeFirstName, lastName, idNumber, jobTitle, annualSalary);
     } // end of conditional
+
+    //empty input fields
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idNumberInput').val('');
+    $('#jobTitleInput').val('');
+    $('#annualSalaryInput').val('');
     
+    //append data onto DOM
+    $('#displayEmployee').append(`
+        <tr>
+            <td>${employeeFirstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSalary}</td>
+            <td> </td>
+        </tr>
+    `)
+
     
 }// end of addEmployee function
 
