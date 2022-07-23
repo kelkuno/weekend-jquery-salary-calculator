1bbe862c5a304d7c82621a601248b2b5ce62f595
Kelsey Kuno <kelsey.kuno@gmail.com>
2022-07-22 20:33:26 -0500

    createEmployee function tested.

--- client.js
+++ client.js
@@ -1,9 +1,25 @@
 $(document).ready(readyNow);
 
-console.log('javascript works');
+// console.log('javascript works');
 
 function readyNow(){
     console.log('jquery works!');
 
 };
 
+let employees = [];
+
+function createEmployee(employeeFirstName, lastName, idNumber, jobTitle, annualSalary){
+    console.log('in the createEmployee function', employeeFirstName, lastName, idNumber, jobTitle, annualSalary )
+    let employee = {
+        employeeFirstName: employeeFirstName,
+        lastName: lastName,
+        idNumber: idNumber,
+        jobTitle: jobTitle,
+        annualSalary: annualSalary
+    }//end of employee object
+    employees.push(employee);
+}// end of newEmployee Function
+
+createEmployee('Kelsey ', 'Kuno', 78, 'driver', 100);
+console.log(employees);
